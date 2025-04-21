573f3a13789b9c3cc68cdaac7f9c78348d17cb56
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-04-21 19:06:11 +0200

    Update elm-review config

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -1,20 +1,11 @@
 module ReviewConfig exposing (config)
 
-{-| Do not rename the ReviewConfig module or the config function, because
-`elm-review` will look for these.
-
-To add packages that contain rules, add them to this review project using
-
-    `elm install author/packagename`
-
-when inside the directory containing this file.
-
--}
-
 import Docs.NoMissing exposing (exposedModules, onlyExposed)
 import Docs.ReviewAtDocs
 import Docs.ReviewLinksAndSections
 import Docs.UpToDateReadmeLinks
+import NoAlways
+import NoBooleanCase
 import NoConfusingPrefixOperator
 import NoDebug.Log
 import NoDebug.TodoOrToString
@@ -25,15 +16,16 @@ import NoMissingTypeAnnotationInLetIn
 import NoMissingTypeExpose
 import NoModuleOnExposedNames
 import NoPrematureLetComputation
+import NoRecordAliasConstructor
 import NoSimpleLetBody
 import NoUnused.CustomTypeConstructorArgs
 import NoUnused.CustomTypeConstructors
 import NoUnused.Dependencies
-import NoUnused.Exports
 import NoUnused.Parameters
 import NoUnused.Patterns
 import NoUnused.Variables
 import Review.Rule as Rule exposing (Rule)
+import ReviewPipelineStyles
 import Simplify
 
 
@@ -44,8 +36,10 @@ config =
         , from = exposedModules
         }
     , Docs.ReviewLinksAndSections.rule
+    , NoAlways.rule
     , Docs.ReviewAtDocs.rule
     , Docs.UpToDateReadmeLinks.rule
+    , NoBooleanCase.rule
     , NoConfusingPrefixOperator.rule
     , NoDebug.Log.rule
     , NoDebug.TodoOrToString.rule
@@ -57,14 +51,21 @@ config =
     , NoMissingTypeExpose.rule
     , NoSimpleLetBody.rule
     , NoPrematureLetComputation.rule
+    , NoRecordAliasConstructor.rule
     , NoUnused.CustomTypeConstructors.rule []
     , NoUnused.CustomTypeConstructorArgs.rule
     , NoUnused.Dependencies.rule
+
     -- , NoUnused.Exports.rule
-    --     |> Rule.ignoreErrorsForFiles [ "src/Glsl/Functions.elm" ]
     , NoUnused.Parameters.rule
     , NoUnused.Patterns.rule
     , NoUnused.Variables.rule
     , Simplify.rule Simplify.defaults
     , NoModuleOnExposedNames.rule
+    , ReviewPipelineStyles.rule
+        [ ReviewPipelineStyles.forbid ReviewPipelineStyles.leftCompositionPipelines
+            |> ReviewPipelineStyles.andCallThem "forbidden << composition"
+        , ReviewPipelineStyles.forbid ReviewPipelineStyles.rightCompositionPipelines
+            |> ReviewPipelineStyles.andCallThem "forbidden >> composition"
+        ]
     ]
