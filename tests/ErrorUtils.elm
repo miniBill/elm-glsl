6501bfa5c216a3b7730291f7fd3d4d8b068db66b
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-04-10 14:05:10 +0200

    Unvendor error utils

--- tests/ErrorUtils.elm
+++ tests/ErrorUtils.elm
@@ -1,110 +1,72 @@
-module ErrorUtils exposing (errorToString, errorsToString, escape, problemToString)
+module ErrorUtils exposing (errorsToString)
 
-import Console
-import Json.Encode
-import List.Extra
-import Parser
-import SortedSet as Set
-
-
-errorsToString : String -> List Parser.DeadEnd -> String
-errorsToString source errors =
-    errors
-        |> List.Extra.gatherEqualsBy (\{ row, col } -> ( row, col ))
-        |> List.map (errorToString source)
-        |> List.Extra.unique
-        |> String.join (Console.black "\n---\n")
+import Ansi.Color
+import Glsl.Parser
+import Parser exposing (Problem)
+import Parser.Advanced exposing (DeadEnd)
+import Parser.Error
 
 
-errorToString : String -> ( Parser.DeadEnd, List Parser.DeadEnd ) -> String
-errorToString source ( error, errors ) =
-    let
-        -- How many lines of context to show
-        context : Int
-        context =
-            4
-
-        lines : List String
-        lines =
-            String.split "\n" source
-                |> List.drop (error.row - context)
-                |> List.take (context * 2)
-                |> List.map (String.replace "\t" " ")
-
-        errorString : String
-        errorString =
-            [ String.repeat (error.col - 1) " "
-            , Console.red "^ "
-            , " at row "
-            , String.fromInt error.row
-            , ", col "
-            , String.fromInt error.col
-            , " "
-            , (error :: errors)
-                |> List.map (\{ problem } -> problemToString problem)
-                |> Set.fromList
-                |> Set.toList
-                |> String.join ", "
-                |> Console.yellow
-            ]
-                |> String.concat
-
-        before : Int
-        before =
-            min error.row context
-    in
-    List.take before lines
-        ++ errorString
-        :: List.drop before lines
-        |> String.join "\n"
+errorsToString :
+    String
+    -> List (DeadEnd Glsl.Parser.Context Problem)
+    -> String
+errorsToString src deadEnds =
+    Parser.Error.renderError
+        { text = identity
+        , formatContext = Ansi.Color.fontColor Ansi.Color.cyan
+        , formatCaret = Ansi.Color.fontColor Ansi.Color.red
+        , newline = "\n"
+        , linesOfExtraContext = 3
+        }
+        { contextStack = contextStackToString
+        , problemToString = Parser.Error.problemToExpected
+        }
+        src
+        deadEnds
+        |> String.concat
 
 
-problemToString : Parser.Problem -> String
-problemToString problem =
-    case problem of
-        Parser.BadRepeat ->
-            "Bad repeat"
-
-        Parser.Expecting something ->
-            "Expecting " ++ something
-
-        Parser.ExpectingInt ->
-            "Expecting int"
-
-        Parser.ExpectingHex ->
-            "Expecting hex"
-
-        Parser.ExpectingOctal ->
-            "Expecting octal"
-
-        Parser.ExpectingBinary ->
-            "Expecting binary"
-
-        Parser.ExpectingFloat ->
-            "Expecting float"
-
-        Parser.ExpectingNumber ->
-            "Expecting number"
-
-        Parser.ExpectingVariable ->
-            "Expecting variable"
-
-        Parser.ExpectingSymbol value ->
-            "Expecting symbol " ++ escape value
-
-        Parser.ExpectingKeyword value ->
-            "Expecting keyword " ++ value
-
-        Parser.ExpectingEnd ->
-            "Expecting end"
-
-        Parser.UnexpectedChar ->
-            "Unexpected char"
-
-        Parser.Problem p ->
-            p
+contextStackToString :
+    DeadEnd Glsl.Parser.Context Problem
+    -> List { row : Int, col : Int, context : String }
+contextStackToString { contextStack } =
+    List.map contextFrameToString contextStack
 
 
-escape : String -> String
-escape input =
-    Json.Encode.encode 0 (Json.Encode.string input)
+contextFrameToString :
+    { row : Int, col : Int, context : Glsl.Parser.Context }
+    -> { row : Int, col : Int, context : String }
+contextFrameToString { row, col, context } =
+    { row = row
+    , col = col
+    , context = contextToString context
+    }
+
+
+contextToString : Glsl.Parser.Context -> String
+contextToString context =
+    case context of
+        Glsl.Parser.ParsingFile ->
+            "Parsing file"
+
+        Glsl.Parser.ParsingFunction ->
+            "Parsing Function"
+
+        Glsl.Parser.ParsingStatement ->
+            "Parsing Statement"
+
+        Glsl.Parser.ParsingExpression ->
+            "Parsing Expression"
+
+        Glsl.Parser.ParsingForInitialization ->
+            "Parsing For Initialization"
+
+        Glsl.Parser.ParsingForCondition ->
+            "Parsing For Condition"
+
+        Glsl.Parser.ParsingForStep ->
+            "Parsing For Step"
+
+        Glsl.Parser.ParsingForBody ->
+            "Parsing For Body"
